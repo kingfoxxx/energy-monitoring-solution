04db508c78cf1da3f0fd0422127f5395191892e7
kingfoxxx <brightally6@gmail.com>
2025-04-19 01:10:05 +0100

    updated

--- terraform/lambda_function.py
+++ terraform/lambda_function.py
@@ -3,6 +3,7 @@ import boto3
 
 firehose = boto3.client('firehose')
 
+# process data on lambda
 def lambda_handler(event, context):
     energy_data = {
         "timestamp": event.get("timestamp"),
